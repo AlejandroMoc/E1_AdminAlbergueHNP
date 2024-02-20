410fdb77821f102e1e53912236406f8fb4fbd5b2
Alejandro M <moctezumaalejandro25@gmail.com>
2024-02-19 23:15:17 -0600

    Inicializada versión sencilla de prueba. Aún no se encuentran separados los componentes.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+//Importar assets
+import logohnp from './assets/logos/logo_hnp.svg';
+
+//Importar CSS
 import './App.css';
 
+//Dibujar elementos
 function App() {
   return (
-    <div className="App">
+    <div className="App-global">
+
+      {/*Barra de navegación*/}
+
+      <nav className="App-navbar">
+        <ul>
+          <a href="#">Inicio</a>
+          <a href="#">Salas</a>
+          <a href="#">Usuarios</a>
+        </ul>
+      </nav>
+
+
+      <title className="App-title">
+        <h1>Administrador del albergue - HNP</h1>
+      </title>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {/* <code>src/App.js</code> */}
+
+        <a className = "App-link" href="#">Administrar salas</a>
+        <a className = "App-link" href="#">Administrar usuarios</a>
+
+        <img src={logohnp} className ="App-logo" alt="logo" />
+
+        {/*Footer*/}
+        <p className = "App-footer">Con tecnología de{' '}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ReactJS
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      </header>    
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//Importar funcion y css
+//TODO cambiar
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
