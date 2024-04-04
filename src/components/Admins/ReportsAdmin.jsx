240b9b46547cf7c1be818b873f962eda0febadf9
christian1444 <chris14_cr7@hotmail.com>
2024-04-03 18:33:53 -0600

    "Commit Inicial"

--- src/components/Admins/ReportsAdmin.jsx
+++ src/components/Admins/ReportsAdmin.jsx
@@ -4,7 +4,6 @@ const ReportsAdmin = () => {
   return (
     <div className='App-minheight'>
       <h1>Administración de Reportes</h1>
-      <p>Aquí va la generación de reportes</p>
     </div>
   )
 }
