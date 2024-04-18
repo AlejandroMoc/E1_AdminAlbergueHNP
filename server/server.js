30c75021b45b950194e78e6322fe1c078e68640c
CsarW <A01656774@tec.mx>
2024-04-17 22:33:26 -0600

    Creo que ya quedo lo de node

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Hola
+app.use(cors());
+app.use(express.json());
+
+/*Conexio a la bd*/
+const pgp=require('pg-promise')();
+const cn={
+    host: 'localhost',
+    port: '5432',
+    database: 'gestionAlbergue',  //Nombre de la base de DATOS
+    user:'postgres',
+    password:'admin123', //Contraseña de PGAdmin
+    allowExitOnIdle:true
+} 
+
+const db = pgp(cn);
+app.get('/posts', (req, res) =>{
+    db.any('SELECT * FROM area')
+    .then((data) => res.json(data))
+    .catch((error) =>console.log('Error: ', error));
+} )
+
+// app.get('/hello', (req, res) => {
+//     res.json({message:"Hola"});
+// })
+
+app.listen(8000, () =>{
+    console.log('Servidor corriendo en el puerto ')
+})
