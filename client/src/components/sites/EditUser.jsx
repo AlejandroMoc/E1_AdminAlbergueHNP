bfdbd49ef8d80857c916b299419819fcfed356e8
Daniela Lozada <a01736594@tec.mx>
2024-05-14 12:59:33 -0600

    CREACIÓN PAGINA EDIT

---
+++ client/src/components/sites/EditUser.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+//Importar componentes
+import EditUserAdmin from '../admins/EditUserAdmin';
+import Footer from '../universal/Footer';
+import { useParams } from "react-router-dom";
+
+const EditUser = () => {
+	const{id_cliente} = useParams();
+	return (
+		<div className="App_global">
+			<EditUserAdmin id_cliente={id_cliente}/>
+			<Footer/>
+		</div>
+	);
+};
+//GG
+export default EditUser;
