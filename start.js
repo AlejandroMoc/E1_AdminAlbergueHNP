eeb3b35c50c446b1d045240134529e35500c6b23
CsarW <A01656774@tec.mx>
2024-05-30 23:03:39 -0600

    Dock más o menos, con ejecutable en segundo plano

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-exec('npm run dev', {cwd: 'server/'}, (err, stdout, stderr) => {
+exec('forever start server.js', {cwd: 'server/'}, (err, stdout, stderr) => {
   if (err) {
     console.error(`Error: ${err}`);
     return;
